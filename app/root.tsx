b5c25d9ae2768d0da899ded158927c84734c3da1
Przemek <przemek@rudzki.ca>
2023-03-08 22:40:44 -0500

    Initial commit

---
+++ app/root.tsx
@@ -0,0 +1,17 @@
+import { LiveReload, Outlet } from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>Notatnik</title>
+      </head>
+      <body>
+        Hello world
+        <Outlet />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
